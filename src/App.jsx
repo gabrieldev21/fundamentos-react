9f32daced5c8fad8f773aa3904e3da899394943a
gabrieldev21 <gabrielp.amorim@live.com>
2021-04-07 22:08:28 -0300

    segunda parte do masterclass

--- src/App.jsx
+++ src/App.jsx
@@ -11,46 +11,65 @@ import Condicional from "./components/basicos/Condicional";
 import CondicionalComIf from "./components/basicos/CondicionalComIf";
 import Pai from "./components/comunicacao/direta/Pai";
 import Super from "./components/comunicacao/indireta/Super";
+import Input from "./components/form/Input";
+import Contador from "./components/contador/Contador";
+import Mega from "./components/mega/Mega";
 
 export default (props) => (
   <div className='App'>
-    <Card titulo='#8 - Comunicação Indireta' color='#000'>
-      <Super></Super>
-    </Card>
+    <h1>Fundamentos React</h1>
 
-    <Card titulo='#7 - Comunicação Direta' color='#4298B5'>
-      <Pai sobrenome='Freitas'></Pai>
-    </Card>
+    <div className='Cards'>
+      <Card titulo='#11 - Mega' color='#73503C'>
+        <Mega qtdeNumero={8}></Mega>
+      </Card>
 
-    <Card titulo='#6 - Condicional v2' color='#FA6900'>
-      <CondicionalComIf numero={2} />
-    </Card>
+      <Card titulo='#10 - Contador' color='#293E6A'>
+        <Contador passo={10} valor={100}></Contador>
+      </Card>
 
-    <Card titulo='#05 - Condicional v1' color='#E94C6F'>
-      <Condicional numero={1} />
-    </Card>
+      <Card titulo='#9 - Input' color='#9C0F5F'>
+        <Input></Input>
+      </Card>
 
-    <Card titulo='#04 - Repetição' color='#008BBA'>
-      <Repeticao />
-    </Card>
+      <Card titulo='#8 - Comunicação Indireta' color='#000'>
+        <Super></Super>
+      </Card>
 
-    <Card titulo='#03 - Componente com Filhos' color='#D96459'>
-      <ComFilhos>
-        <ul>
-          <li>Ana</li>
-          <li>Bia</li>
-          <li>Carlos</li>
-          <li>Zé</li>
-        </ul>
-      </ComFilhos>
-    </Card>
+      <Card titulo='#7 - Comunicação Direta' color='#4298B5'>
+        <Pai sobrenome='Freitas'></Pai>
+      </Card>
 
-    <Card titulo='#02 -Componente com Parametro' color='#FF85CB'>
-      <ComParametro titulo='esse é o título' subtitulo='este é o subtitulo' />
-    </Card>
+      <Card titulo='#6 - Condicional v2' color='#FA6900'>
+        <CondicionalComIf numero={2} />
+      </Card>
 
-    <Card titulo='#01 - Primeiro componente' color='#92B06A'>
-      <Primeiro />
-    </Card>
+      <Card titulo='#05 - Condicional v1' color='#E94C6F'>
+        <Condicional numero={1} />
+      </Card>
+
+      <Card titulo='#04 - Repetição' color='#008BBA'>
+        <Repeticao />
+      </Card>
+
+      <Card titulo='#03 - Componente com Filhos' color='#D96459'>
+        <ComFilhos>
+          <ul>
+            <li>Ana</li>
+            <li>Bia</li>
+            <li>Carlos</li>
+            <li>Zé</li>
+          </ul>
+        </ComFilhos>
+      </Card>
+
+      <Card titulo='#02 -Componente com Parametro' color='#FF85CB'>
+        <ComParametro titulo='esse é o título' subtitulo='este é o subtitulo' />
+      </Card>
+
+      <Card titulo='#01 - Primeiro componente' color='#92B06A'>
+        <Primeiro />
+      </Card>
+    </div>
   </div>
 );
---
+++ src/components/contador/Display.jsx
@@ -0,0 +1,6 @@
+/* eslint-disable import/no-anonymous-default-export */
+import React from "react";
+
+export default (props) => {
+  return <h4>Valor: {props.valor}</h4>;
+};
